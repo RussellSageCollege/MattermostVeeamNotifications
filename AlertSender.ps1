c7b43f9492529f7e0c2e0d96edb7591899dbf380
Alex Markessinis <markea@sage.edu>
2017-01-10 21:45:23 -0500

    renamed config file

--- AlertSender.ps1
+++ AlertSender.ps1
@@ -9,7 +9,7 @@ Param(
 Import-Module "$PSScriptRoot\Helpers"
 
 # Get the config from our config file
-$config = (Get-Content "$PSScriptRoot\config\vsn.json") -Join "`n" | ConvertFrom-Json
+$config = (Get-Content "$PSScriptRoot\config\config.json") -Join "`n" | ConvertFrom-Json
 
 # Should we log?
 if($config.debug_log) {
